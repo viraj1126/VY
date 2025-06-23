f47c684cfc767740c9e0e4e734dbcaec86133a13
YASH GAIKWAD <YASH GAIKWAD@DESKTOP-OUH6CNH>
2025-06-23 12:45:52 +0530

    By Yash

--- crudspring-1/src/main/java/com/service/Servicei.java
+++ crudspring-1/src/main/java/com/service/Servicei.java
@@ -1,6 +1,8 @@
 package com.service;
 
 
+import java.util.List;
+
 import com.model.Student;
 
 public interface Servicei 
--- crudspring-1/src/main/java/com/serviceimpl/Serviceimpl.java
+++ crudspring-1/src/main/java/com/serviceimpl/Serviceimpl.java
@@ -1,5 +1,7 @@
 package com.serviceimpl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.model.Student;
