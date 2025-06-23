0192ed700d768fc5df1da757123fe88d5ecaa757
YASH GAIKWAD <YASH GAIKWAD@DESKTOP-OUH6CNH>
2025-06-23 12:11:20 +0530

    By Yash

--- crudspring-1/src/main/java/com/controller/HomeController.java
+++ crudspring-1/src/main/java/com/controller/HomeController.java
@@ -3,8 +3,11 @@ package com.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.model.Student;
 import com.service.Servicei;
 
 @RestController
@@ -12,6 +15,13 @@ public class HomeController {
 
 	@Autowired
 	Servicei si;
+	
+	@PutMapping("/student/{id}")
+	public Student updateData(@RequestBody Student s,@PathVariable int id) 
+	{
+		return si.updateData(s,id);
+		
+	}
 	
 	@DeleteMapping("/student/{id}")
 	public void delete(@PathVariable int id)
